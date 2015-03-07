a94be81adf8430f8274c5c3f879a9ae6acbca113
kgiszewski <kevin@giszewski.com>
2015-03-07 09:35:39 -0500

    Add author to feed

--- src/Feed/main.js
+++ src/Feed/main.js
@@ -2,11 +2,13 @@
     "popular" : [
         {
             "title": "Official Umbraco Documentation",
-            "url": "https://github.com/umbraco/Umbraco4Docs/archive/master.zip"
+            "url": "https://github.com/umbraco/Umbraco4Docs/archive/master.zip",
+            "author": "Umbraco Team"
         },
         {
             "title": "Learn Umbraco 7",
-            "url": "https://github.com/kgiszewski/LearnUmbraco7/archive/master.zip"
+            "url": "https://github.com/kgiszewski/LearnUmbraco7/archive/master.zip",
+            "author": "Kevin Giszewski"
         }
     ]
 }
