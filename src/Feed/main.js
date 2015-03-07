858ee4c16b8c95b90a5cb5b2306d1df6910d6209
kgiszewski <kevin@giszewski.com>
2015-03-07 10:57:52 -0500

    Modify feed, give up on file upload

--- src/Feed/main.js
+++ src/Feed/main.js
@@ -2,13 +2,15 @@
     "popular" : [
         {
             "title": "Official Umbraco Documentation",
-            "url": "https://github.com/umbraco/Umbraco4Docs/archive/master.zip",
-            "author": "Umbraco Team"
+            "gitHubUser":"umbraco",
+            "gitHubRepo":"Umbraco4Docs",
+            "branch":"master"
         },
         {
             "title": "Learn Umbraco 7",
-            "url": "https://github.com/kgiszewski/LearnUmbraco7/archive/master.zip",
-            "author": "Kevin Giszewski"
+            "gitHubUser":"kgiszewski",
+            "gitHubRepo":"LearnUmbraco7",
+            "branch":"master"
         }
     ]
 }
