673651104c044a3ec387de31eef0a750c56827d9
kgiszewski <kevin@giszewski.com>
2015-07-01 12:37:36 -0400

    Mockup UI for search

---
+++ src/App_Plugins/UmbracoBookshelf/js/search.controller.js
@@ -0,0 +1,34 @@
+﻿angular.module('umbraco').controller('umbracoBookshelfSearchController', function ($scope, umbracoBookshelfResource) {
+
+    $scope.model = {};
+    $scope.model.keywords = "";
+
+    $scope.model.books = [
+        {
+            name: "Learn Umbraco 7",
+            results: [
+                {
+                    name: "04 - Surface, RenderMVC and API Controllers",
+                    url: "#"
+                },
+                {
+                    name: "06 - Blah Chapter",
+                    url: "#"
+                }
+            ]
+        },
+        {
+            name: "Official Umbraco Docs",
+            results: [
+                {
+                    name: "Surface Controllers",
+                    url: "#"
+                },
+                {
+                    name: "API Controllers",
+                    url: "#"
+                }
+            ]
+        }
+    ];
+});
